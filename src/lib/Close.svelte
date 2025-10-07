c70570141bf5e7b5b0997ba7f9baa8f746dd40e6
Fabio Riva <f.riva@sotefin.ch>
2025-10-07 15:02:23 +0200

    fix: missing translation error

--- src/lib/Close.svelte
+++ src/lib/Close.svelte
@@ -25,7 +25,7 @@
 </script>
 
 <!-- <Alert message={messages[mesg.close]} role="warning" title={$t("title.action")} /> -->
-<Alert message={$t(`close.mesg.${Number(mesg.close)}`)} role="warning" title={$t('action')} />
+<Alert message={$t(`close.mesg.${Number(mesg.close)}`)} role="warning" title={$t('title.action')} />
 <div class="h-[443px] flex items-center justify-center">
 	<button
 		class={`w-[256px] h-[256px] cursor-pointer rounded-full bg-yellow-500 border-4 border-yellow-600 opacity-100 shadow-2xl shadow-slate-700 font-bold text-2xl text-amber-700 uppercase ${isPressed && 'bg-gradient-to-br from-amber-500 to-amber-600 text-amber-600'}`}
