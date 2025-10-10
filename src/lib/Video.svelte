4e4f2b99bffb4ad5dd6a2571603a91772d148227
Fabio Riva <f.riva@sotefin.ch>
2025-10-10 17:08:23 +0200

    video src changed

--- src/lib/Video.svelte
+++ src/lib/Video.svelte
@@ -16,5 +16,6 @@
 		id="kiosk-video"
 	></video>
 {:else}
-	<video autoplay loop muted src="aps.mp4" class="px-0" id="kiosk-video"></video>
+	<!-- <video autoplay loop muted src="aps.mp4" class="px-0" id="kiosk-video"></video> -->
+	<video autoplay loop muted src="https://sveltejs.github.io/assets/caminandes-llamigos.mp4" class="px-0" id="kiosk-video"></video>
 {/if}
