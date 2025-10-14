281901eda6f3cfc4dec007f72755299d334dc01f
Fabio Riva <f.riva@sotefin.ch>
2025-10-14 11:09:38 +0200

    uploaded aps.mp4 video

--- src/lib/Video.svelte
+++ src/lib/Video.svelte
@@ -16,6 +16,5 @@
 		id="kiosk-video"
 	></video>
 {:else}
-	<!-- <video autoplay loop muted src="aps.mp4" class="px-0" id="kiosk-video"></video> -->
-	<video autoplay loop muted src="https://sveltejs.github.io/assets/caminandes-llamigos.mp4" class="px-0" id="kiosk-video"></video>
+	<video autoplay loop muted src="aps.mp4" class="px-0" id="kiosk-video"></video>
 {/if}
